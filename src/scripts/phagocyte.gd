f7c5be936290f0c4a5031656a9318785fb39517e
shaboy20 <138219481+shaboy20@users.noreply.github.com>
2026-06-17 19:01:29 +0530

    stunned phagocyte animation

--- src/scripts/phagocyte.gd
+++ src/scripts/phagocyte.gd
@@ -20,6 +20,7 @@ func take_damage(num : int):
 	print("enemy health " , health)
 	take_knockback()
 	is_stunned = true
+	sprite.play("damaged")
 	await get_tree().create_timer(2).timeout
 	is_stunned = false
 	
@@ -43,10 +44,12 @@ func _physics_process(delta: float) -> void:
 	if player == null:
 		return
 	
-	if sprite.is_playing() and sprite.animation == "attack" and not is_attacking:
+	if sprite.is_playing() and sprite.animation == "attack" and not is_attacking and not is_stunned:
 		pass
-	else:
+	elif not is_stunned:
 		sprite.play("default")
+	
+	
 		
 		var direction_to_player = (player.global_position - global_position).normalized()
 		if roll < 0.002:
